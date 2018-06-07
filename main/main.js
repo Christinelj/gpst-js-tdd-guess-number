69df4be36ea6d9ecc7602c2a92bcd8201cec8318
Christinelj <345236126@qq.com>
2018-06-07 15:53:16 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,6 +1,34 @@
-// Write your cade below:
-function main() {};
+function main(input) {
+    var compareResult = '';
+    var numberA = 0;
+    var numberB = 0;
+    var systemInput = input.system.split('');
+    var userInput = input.user.split('');
+    for (var i = 0; i < systemInput.length; i++) {
+        for (var j = i; j < userInput.length; j++) {
+            if (systemInput[i] === userInput[j]) {
+                numberA++;
+                break;
+            } else {
+                for (var k = 0; k < userInput.length; k++) {
+                    if (systemInput[i] === userInput[k]) {
+                        numberB++;
+                        break;
+                    }
+                }
+            }
+            break;
+        }
+    }
 
-module.exports = {
-    main
-}
+    function printResult(numberA, numberB) {
+        var compareResult = '';
+        compareResult = numberA + 'A' + numberB + 'B';
+        return compareResult;
+    }
+
+    compareResult = printResult(numberA, numberB);
+    return compareResult;
+};
+
+module.exports = main;
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,23 @@
 const main = require('../main/main');
 
-describe('main()', () => {
+describe('main()', function () {
+    var compare_a = "4A0B";
+    var compare_b = "0A4B";
+    var compare_c = "0A0B";
+
+    it('print game result', function () {
+        var result = main({ system: '1234', user: '1234' });
+        expect(result).toEqual(compare_a);
+    });
+
+    it('print game result', function () {
+        var result = main({ system: '1234', user: '4321' });
+        expect(result).toEqual(compare_b);
+    });
+
+    it('print game result', function () {
+        var result = main({ system: '1234', user: '5678' });
+        expect(result).toEqual(compare_c);
+    });
 
 });
